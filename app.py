5c3d429fe312ed66d4744b1c5156efab3b3a1ea4
cypher-spec <73258889+cypher-spec@users.noreply.github.com>
2020-10-26 16:57:14 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 from chatbot import chatbot
 from flask import Flask, render_template, request
+from flask import jsonify
+import json
+from waitress import serve
+
+a = {"resposne": ""}
 
 app = Flask(__name__)
 app.static_folder = 'static'
@@ -8,10 +13,13 @@ app.static_folder = 'static'
 def home():
     return render_template("index.html")
 
-@app.route("/get")
+@app.route("/get_bot_response", methods=[ 'GET'])
 def get_bot_response():
-    userText = request.args.get('msg')
-    return str(chatbot.get_response(userText))
+    if request.method == 'GET':
+        userText = request.args.get('msg')
+        response = str(chatbot.get_response(userText))
+        print(response)
+        return response
 
 if __name__ == "__main__":
     app.run() 
